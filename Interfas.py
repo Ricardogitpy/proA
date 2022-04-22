71bc5a1c6dbae5d1e2da0af2856b8cf33a5c5305
Ricardo Aaron <ricardocorreoperez@gmail.com>
2022-04-21 20:06:48 -0500

    Vercion f

--- Interfas.py
+++ Interfas.py
@@ -1,18 +1,21 @@
+#impotar librerias
+from socket import TCP_QUICKACK
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-from turtle import heading
+import tkinter.font as tkFont
 import os
-import re  
+import re
+from tkinter import font
+from matplotlib.pyplot import text  
 from yaml import load
 import fitz
 from googlesearch import search
 import wget
 
-
 def  pdf_text(ruta):
       texto = ""
-      
+      t.delete(0,END)
       documento = fitz.open(ruta)
       for i  in range( documento.pageCount):
          pagina  = documento.loadPage(i)
@@ -38,18 +41,17 @@ def get_value():
    results = search(e_text)
    for r in results:
        link = r
-       var = os.path.exists("/home/ricardo/Documentos/proyecto_Automatas/miarchivo.pdf") 
+       var = os.path.exists("/home/ricardo/Documentos/proyecto_Automatas/miarchivo.pdf")  
        if (var == True): 
             os.remove("/home/ricardo/Documentos/proyecto_Automatas/miarchivo.pdf")
        wget.download(link,"/home/ricardo/Documentos/proyecto_Automatas/miarchivo.pdf")
        ruta = "/home/ricardo/Documentos/proyecto_Automatas/miarchivo.pdf"
-       y = pdf_text(ruta)
+       y = pdf_text(ruta)            #<---- llamada a la  funcion pdf_text 
        if (r  == r):
            break
    print (link)
 
 def ExpresionR():
-      print("si funciono")
       ruta = "/home/ricardo/Documentos/proyecto_Automatas/miarchivo.pdf"
       texto = ""
       documento = fitz.open(ruta)
@@ -57,30 +59,26 @@ def ExpresionR():
          pagina  = documento.loadPage(i)
          texto = texto + pagina.getText() 
       getText = str(Palabra.get())
-      if re.search(r'^[a-zA-Z]+$',getText):
-        # Validacion para que se ingrese solo una palabra usando una ER para detectar espacios.
-          if re.search(r'[ ]',getText):
-             y = ttk.Label(root, text="palabra invalida").place(x=550,y=300)
-          else:
-             patron = re.compile(getText)
-             fa = patron.findall(texto)
-             x = str(len(fa))
-             y = ttk.Label(root, text="el numero de palabras encontradas es:"+x).place(x=480,y=300)
+      if re.search(r'^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$',getText):
+         patron = re.compile(getText)
+         fa = patron.findall(texto)
+         x = str(len(fa))
+         y = ttk.Label(root, text="el nùmero de palabras encontradas es:"+ x, background="#525E75" ,foreground="#F1DDBF").place(x=480,y=330)
       else:
-         y = ttk.Label(root, text="palabra invalida").place(x=550,y=300)
+         y = ttk.Label(root, text="***********palabra invalida***********" ,background="#525E75",foreground="#F1DDBF").place(x=480,y=330)
 
-
-
-root = Tk()
-root.geometry("770x450")
-ttk.Label(root,text="documento a buscar:").place(x=350,y=0)
+# interfaz
+root = tk.Tk()
+root.geometry("770x500")
+root.configure(background="#525E75")
+tk.Label(root,text="Buscador web: ",font=("Arial",16),bg="#525E75",fg="#F1DDBF").place(x=340,y=0)
 Buscador = tk.StringVar()
-ttk.Entry(root, textvariable=Buscador ,width= 50).place(x=250,y=20)
-ttk.Button(root,text="Buscar", command= get_value ).place(x=350,y=45)
+tk.Entry(root, textvariable=Buscador ,width= 50).place(x=230,y=25)
+tk.Button(root,text="Buscar" , font=("Arial",16),bg="#92BA92" ,command= get_value ).place(x=350,y=45)
 t = Listbox(root ,width= 60,height=30)
-t.place(x=50,y=75)
-ttk.Label(root,text="palabra:").place(x=550,y=200)
+t.place(x=50,y=90)
+tk.Label(root,text=" Palabra:",font=("Arial",16),fg="#F1DDBF",bg="#525E75").place(x=550,y=200)
 Palabra = tk.StringVar()
-ttk.Entry(root,textvariable= Palabra ,width= 30).place(x=480,y=235)
-ttk.Button(root,text="Buscar",command=ExpresionR ).place(x=550,y=270)
+ttk.Entry(root,textvariable= Palabra ,width= 30).place(x=500,y=235)
+tk.Button(root,text="Buscar",font=("Arial",14),bg="#92BA92",command=ExpresionR ).place(x=550,y=260)
 root.mainloop()
